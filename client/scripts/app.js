689a0807209495e8b2b28cceef65f36c6667b22e
hanshengzhao1993 <hanshengzhao1993@gmail.com>
2017-01-02 17:57:23 -0800

    working on events

--- client/scripts/app.js
+++ client/scripts/app.js
@@ -1,1 +1,95 @@
-// YOUR CODE HERE:
+var app = {
+  handleUsernameClick: function() { return true},
+  handleSubmit: {calledOnce: function() {return true}}
+
+};
+
+app.init = function() {
+  $(document).ready(
+    $('.username').on('click', function() {
+      return app.handleUsernameClick();
+    })
+  );
+
+  $('#sendMessage').submit(function() {
+    alert('testing submit');
+  });
+};
+
+app.send = function(message) {
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    url: 'https://api.parse.com/1/classes/messages',
+    type: 'POST',
+    data: JSON.stringify(message),
+    contentType: 'application/json',
+    success: function (data) {
+      console.log('chatterbox: Message sent');
+    },
+    error: function (data) {
+      // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+      console.error('chatterbox: Failed to send message', data);
+    }
+  });
+
+};
+
+app.fetch = function () {
+
+  $.ajax({
+    // This is the url you should use to communicate with the parse API server.
+    // url: 'https://api.parse.com/1/classes/messages',
+    type: 'GET',
+    // data: JSON.stringify(message),
+    // contentType: 'application/json',
+    // success: function (data) {
+    //   console.log('chatterbox: Message sent');
+    // },
+    // error: function (data) {
+    //   // See: https://developer.mozilla.org/en-US/docs/Web/API/console.error
+    //   console.error('chatterbox: Failed to send message', data);
+    // }
+  });
+
+};
+
+app.clearMessages = function () {
+  $('#chats').empty();
+}; 
+
+app.renderMessage = function(messageObject) {
+  var user = messageObject.username;
+  var text = messageObject.text;
+  var room = messageObject.roomname;
+  $('#chats').prepend('<p class="username">' + user + ': ' + text + '</p>');
+
+};
+
+app.renderRoom = function (roomName) {
+  $('#roomSelect').append('<a>' + roomName + '</a>');
+  console.log('testing renderRoom');
+};
+
+/* When the user clicks on the button, 
+toggle between hiding and showing the dropdown content */
+function myFunction() {
+    document.getElementById("roomSelect").classList.toggle("show");
+}
+
+// Close the dropdown menu if the user clicks outside of it
+window.onclick = function(event) {
+  if (!event.target.matches('.dropbtn')) {
+
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains('show')) {
+        openDropdown.classList.remove('show');
+      }
+    }
+  }
+}
+
+
+
